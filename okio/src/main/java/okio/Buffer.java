ee74f7413f6edbb35ea317e5800a3350a0f3dbf8
Oskar Olofsson <oskarolo@kth.se>
2018-02-25 22:08:18 +0100

    fix: trying to fix tests

--- okio/src/main/java/okio/Buffer.java
+++ okio/src/main/java/okio/Buffer.java
@@ -1616,11 +1616,8 @@ public final class Buffer implements BufferedSource, BufferedSink, Cloneable, By
   public ByteString sha512() {
       return digest("SHA-512");
   }
-  /** Returns exception . */
-  public ByteString wrongAlgorithm() {
-    return digest("wrong");
-  }
-  private ByteString digest(String algorithm) {
+
+  public ByteString digest(String algorithm) {
     try {
       MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
       if (head != null) {
